e5e29e1bff5ddcc7e4e3aa6d153916cd802c708d
Erik Armijos <earmijoss1@est.ups.edu.ec>
2022-01-18 08:20:57 -0500

    Agregar productos y calculos

--- tienda.cpp
+++ tienda.cpp
@@ -1,6 +1,8 @@
 #include "tienda.h"
 #include "ui_tienda.h"
 
+#include <qdebug.h>
+
 Tienda::Tienda(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Tienda)
@@ -13,9 +15,11 @@ Tienda::Tienda(QWidget *parent)
         ui->inProducto->addItem(p->nombre());
     }
     // Configurar cabecera de la tabla
-    QStringList cabecera = {"Cantidad","Producto","Subtotal"};
-    ui->outDetalle->setColumnCount(3);
+    QStringList cabecera = {"Cantidad","Producto","P. Unitario","Subtotal"};
+    ui->outDetalle->setColumnCount(4);
     ui->outDetalle->setHorizontalHeaderLabels(cabecera);
+    // Inicializaaar las variables
+    m_subtotal = 0;
 }
 
 Tienda::~Tienda()
@@ -34,4 +38,58 @@ void Tienda::cargarProductos()
     // Podria leerse de una base de datos, de un archivo o incluso de internet
 
 }
+
+void Tienda::calcular(float stProducto)
+{
+    // Calcular valores
+    m_subtotal += stProducto;
+    float iva = m_subtotal * IVA/100;
+    float total= m_subtotal + iva;
+    // Mostrar valores en GUI
+    ui->outSubtotal->setText("$ " + QString::number(m_subtotal, 'f', 2));
+    ui->outIVA->setText("$ " + QString::number(iva, 'f', 2));
+    ui->outTotal->setText("$ " + QString::number(total, 'f', 2));
+}
+
+void Tienda::on_inProducto_currentIndexChanged(int index)
+{
+    // Obtener el precio del producto actual seleccionado
+    float precio = m_productos.at(index)->precio();
+    // Mostrar el precio del producto en la etiqueta
+    ui->outPrecio->setText("$ " + QString::number(precio, 'f',2));
+    // Resetear el spin box de cantidad
+    ui->inCantidad->setValue(0);
+}
+
+void Tienda::on_btnAgregar_released()
+{
+    // Validar que no se agregen con 0 cantidad
+    int cantidad = ui -> inCantidad->value();
+    if(cantidad == 0){
+        return;
+    }
+    // Obtener los datos de la GUI
+    int i = ui->inProducto->currentIndex();
+    Producto *p = m_productos.at(i);
+
+    // Calcular el subtotal del producto
+    float subTotal = p->precio() * cantidad;
+    qDebug() << p->nombre() << cantidad << p->precio() << subTotal;
+
+    // Agregar datos a la tabla
+    int fila = ui->outDetalle->rowCount();
+    ui->outDetalle->insertRow(fila);
+    ui->outDetalle->setItem(fila, 0, new QTableWidgetItem(QString::number(cantidad)));
+    ui->outDetalle->setItem(fila, 1, new QTableWidgetItem(p->nombre()));
+    ui->outDetalle->setItem(fila, 2, new QTableWidgetItem(QString::number(p->precio(), 'f',2)));
+    ui->outDetalle->setItem(fila, 3, new QTableWidgetItem(QString::number(subTotal, 'f',2)));
+
+    // Limpiar datos
+    ui->inCantidad->setValue(0);
+    ui->inProducto->setFocus();
+
+    // Actualizar subtotales
+    calcular(subTotal);
+}
+
 
